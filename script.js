724501d0b2b21c53233e4ae00fa0a15e27873cc6
✦Sithija Dewmina✦ <92771213+Sithijadewmina@users.noreply.github.com>
2022-01-30 09:07:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,30 +26,6 @@ const music_list = [
         name : 'ගුත්තිල වෙන නද Rap',
         artist : 'Unknown Artist',
         music : 'music/audio-1746346543.mp3'
-    },
-    {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
-    },
-    {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
-    },
-    {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
     }
 ];
 
